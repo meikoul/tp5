8d0a066248b5b7e5688e3d96200bcce7321864bb
meikoul <meikoul@163.com>
2016-09-23 16:33:42 +0800

    修改数据库配置

--- application/database.php
+++ application/database.php
@@ -13,15 +13,15 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => '127.0.0.1',
+    'hostname'       => '106.75.139.87',
     // 数据库名
     'database'       => 'tp5',
     // 用户名
     'username'       => 'root',
     // 密码
-    'password'       => 'root',
+    'password'       => '123456',
     // 端口
-    'hostport'       => '3306',
+    'hostport'       => '3389',
     // 连接dsn
     'dsn'            => '',
     // 数据库连接参数
